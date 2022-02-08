e0c21851152935ded7af25b64b973dc2e0ba86bc
tarek-jatri <tarek.jatri@gmail.com>
2022-02-08 18:28:32 +0600

    #18 Request Object - ExpressJS Done Except Coding

--- expressjs_tutorial/index.js
+++ expressjs_tutorial/index.js
@@ -1,16 +1,45 @@
 const express = require("express");
 const app = express();
 
-app.use(express.raw());
+// app.use(
+//   express.static(`${__dirname}/public/`, {
+//     index: "home.html",
+//   })
+// );
 
-app.get("/", (req, res) => {
-  res.send("This is home page");
+app.set("view engine", "ejs");
+
+app
+  .route("/about/mission")
+  .get((req, res) => {
+    res.render("pages/about");
+  })
+  .post((req, res) => {
+    res.send("Welcome to application home post");
+  })
+  .put((req, res) => {
+    res.send("Welcome to application home put");
+  });
+
+/**
+ * app.param()
+app.param("id", (req, res, next, id) => {
+  //  creating user
+  const user = {
+    userid: id,
+    name: "Abir Hossain",
+  };
+
+  req.user = user;
+  next();
 });
 
-app.post("/", (req, res) => {
-  console.log(typeof req.body);
+app.get("/user/:id", (req, res) => {
+  console.log(req.user);
   res.send("This is home page using post");
 });
+
+ */
 
 app.listen(3030, () => {
   console.log("Connection listening on 3030");
