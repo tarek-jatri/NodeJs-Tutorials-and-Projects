c635f8ffe9248ef8e4b199ca73c69074acf5756b
tarek-jatri <tarek.jatri@gmail.com>
2022-02-15 16:51:57 +0600

    JWT

--- express-crud-application/index.js
+++ express-crud-application/index.js
@@ -1,23 +1,24 @@
-//=> Imporitng
+//=> Importing
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const todoHandler = require("./routeHandlers/todoHandler");
+const userHandler = require("./routeHandlers/userHandler");
 
 //=> express app initialization
 const app = express();
+dotenv.config();
 app.use(express.json());
 
 //=> database connection with mongoose
 mongoose
-  .connect("mongodb://localhost/todos", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/todos")
   .then(() => console.log("Connection Successfull"))
   .catch((err) => console.log(err));
 
 //=> application routes
 app.use("/todo", todoHandler);
+app.use("/user", userHandler);
 
 //=> default error handler
 function errorHandler(err, req, res, next) {
---
+++ express-crud-application/routeHandlers/userHandler.js
@@ -0,0 +1,83 @@
+//=> Importing
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const userSchema = require("../schemas/userSchema");
+
+//=> Router Setup
+const router = express.Router();
+
+//=> Creating model of user schema
+const User = mongoose.model("User", userSchema);
+
+//=> Routing
+
+// SIGNUP
+router.post("/signup", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(200).json({
+      message: "User signed up successfully!!!",
+    });
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json({
+      error: "Failed to sign up !!!",
+    });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    // check if username exists or not
+    const user = await User.find({ username: req.body.username });
+    console.log(user);
+    if (user && user.length > 0) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user[0].password
+      );
+      console.log(">>>> ", isValidPassword);
+      if (isValidPassword) {
+        //  Generate Token
+        const token = jwt.sign(
+          {
+            userId: user[0]._id,
+            username: user[0].username,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+        res.status(200).json({
+          access_token: token,
+          message: "Login Successfull",
+        });
+      } else {
+        res.status(401).json({
+          error: "Authentication Error!!!",
+        });
+      }
+    } else {
+      res.status(401).json({
+        error: "Authentication Error!!!",
+      });
+    }
+  } catch {
+    res.status(401).json({
+      error: "Authentication Error!!!",
+    });
+  }
+});
+
+//=> Exporting
+module.exports = router;
