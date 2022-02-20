ad85e3de42e49d608710f19a22525bdd49718b4a
tarek-jatri <tarek.jatri@gmail.com>
2022-02-21 02:09:52 +0600

    #33 Project(part 4) upto login middlewares done - upto 20 min

--- chat-application/controller/Login/loginController.js
+++ chat-application/controller/Login/loginController.js
@@ -1,9 +1,74 @@
+// external imports
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+// internal imports
+const User = require("../../models/People");
+
 // get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+
+// login user
+async function login(req, res, next) {
+  try {
+    // find a user who has the email/phone
+    const user = await User.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+    });
+    if (user && user._id) {
+      // check if inserted password is valid
+      const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+      if (isValidPassword) {
+        // prepare the user object to generate token 
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        }
+        // generate the token
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRY
+        });
+
+        // set the token into cookie
+        res.cookie(process.env.COOKIE_NAME, token, {
+          maxAge: process.env.JWT_EXPIRY,
+          httpOnly: true,
+          signed: true,
+        })
+
+        // set logged user infos 
+        res.locals.loggedInUser = userObject;
+
+        res.render("inbox");
+
+      } else {
+        throw createError("Login failed! Please try again...")
+      }
+    } else {
+      throw createError("Login failed! Please try again...")
+    }
+  } catch (error) {
+    res.render("index", {
+      data: {
+        username: req.body.username,
+      },
+      errors: {
+        common: {
+          msg: error.message,
+        }
+      }
+    });
+  }
+}
+
 // exporting
 module.exports = {
   getLogin,
+  login,
 };
--- chat-application/router/loginRouter.js
+++ chat-application/router/loginRouter.js
@@ -2,15 +2,23 @@
 const express = require("express");
 
 //=> internal imports
-const { getLogin } = require("../controller/Login/loginController");
+const { getLogin, login } = require("../controller/Login/loginController");
 const documentHtmlResponse = require("../middlewares/common/documentHtmlResponse");
+const { doLoginValidators, doLoginValidationHandler } = require("../middlewares/login/loginValidator");
+
+
+const page_title = "Login";
+
 
 //=>
 const router = express.Router();
 
 //=> setting up the routing
 // login page
-router.get("/", documentHtmlResponse("Login"), getLogin);
+router.get("/", documentHtmlResponse(page_title), getLogin);
+
+// login page
+router.post("/", documentHtmlResponse(page_title), doLoginValidators, doLoginValidationHandler, login);
 
 // exporting
 module.exports = router;
